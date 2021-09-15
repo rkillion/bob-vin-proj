1ad236698d16dfa7342555b812ea326fbd2adf0d
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-15 16:37:53 -0400

    Game details done on form

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 const FormBox = styled.form`
@@ -19,6 +19,10 @@ const FormSection = styled.div`
     border: 1px solid black;
 `;
 
+const FormSectionHeader = styled.h2`
+    
+`;
+
 const FormRow = styled.div`
     display: flex;
     flex-flow: row wrap;
@@ -31,52 +35,116 @@ const SelectorInput = styled.select`
 `;
 
 const TextInput = styled.input`
-    type: text;
+`;
+
+const TextAreaInput = styled.textarea`
+    width: 70%;
+    min-width: 200px;
+    min-height: 100px;
+`;
+
+const CheckBox = styled.input`
+`;
+
+const InputLabel = styled.label`
+
 `;
 
 function SubmitForm({ developers }) {
     const [formData,setFormData] = useState({
-        developer: "",
-        developer_github: ""
+        developer: {
+            name: "",
+            github_profile: ""
+        },
+        title: "",
+        github_link: "",
+        preview_href: "",
+        description: "",
+        language: "",
+        ["has_comments?"]: false,
+        file_size: 0,
+        ["keyboard?"]: false,
+        ["controller?"]: false,
+        game_features: {
+            feature_explanation: "",
+            feature: ""
+        }
     });
 
     const handleSubmit = e => {
         e.preventDefault();
         //add the developer to the list in state
         //add the game to the list in state
-        console.log("Select value is: ",e.target.selectedDeveloper.value)
         console.log(formData)
     }
 
     const handleFormChange = e => {
         let field;
+        let subfield;
+        let checkbox = false;
         switch (e.target.name) {
             case "newDeveloper" :
                 field = "developer";
+                subfield = "name";
                 break;
             case "selectedDeveloper" :
                 field = "developer";
+                subfield = "name";
                 break;
             case "newDeveloperGithub" :
-                field = "developer_github";
+                field = "developer";
+                subfield = "github_profile";
+                break;
+            case "title" :
+                field = "title";
+                break;
+            case "githubLink" :
+                field = "github_link";
+                break;
+            case "previewHref" :
+                field = "preview_href";
+                break;
+            case "language" :
+                field = "language";
+                break;
+            case "description" :
+                field = "description";
+                break;
+            case "hasComments?" :
+                field = "has_comments?";
+                checkbox = true;
+                break;
+            case "keyboard?" :
+                field = "keyboard?";
+                checkbox = true;
+                break;
+            case "controller?" :
+                field = "controller?";
+                checkbox = true;
                 break;
             default:
                 field = undefined;
         }
         let newData = JSON.parse(JSON.stringify(formData));
-        newData[field] = e.target.value
+        if(subfield) {
+            newData[field][subfield] = e.target.value;
+        } else if (checkbox) {
+            newData[field] = e.target.checked;
+        } else {
+            newData[field] = e.target.value
+        }
         setFormData(newData);
     }
 
     return (
         <FormBox onSubmit={handleSubmit}>
             <FormSection>
-                <h2>Developer Info</h2>
+                <FormSectionHeader>Developer Info</FormSectionHeader>
                 <FormRow>
                     <p>Select a Developer: </p>
                     <SelectorInput
                         name="selectedDeveloper"
-                        value={formData.developer}
+                        value={formData.developer.name}
                         onChange={handleFormChange}
                     >
                         <option>{""}</option>
@@ -88,14 +156,91 @@ function SubmitForm({ developers }) {
                     <p>New Developer: </p>
                     <TextInput 
                         name="newDeveloper"
-                        value={formData.developer}
+                        value={formData.developer.name}
                         onChange={handleFormChange}
+                        placeholder="Name"
                     />
                     <TextInput 
                         name="newDeveloperGithub"
-                        value={formData.developer_github}
+                        value={formData.developer.github_profile}
                         onChange={handleFormChange}
+                        placeholder="Github Profile Link"
                     />
+                </FormRow>
+            </FormSection>
+            <FormSection>
+                <FormSectionHeader>Game Details</FormSectionHeader>
+                <FormRow>
+                    <TextInput 
+                        name="title"
+                        value={formData.title}
+                        onChange={handleFormChange}
+                        placeholder="Title"
+                    />
+                    <TextInput 
+                        name="githubLink"
+                        value={formData.github_link}
+                        onChange={handleFormChange}
+                        placeholder="Github Link"
+                    />
+                    <TextInput 
+                        name="previewHref"
+                        value={formData.preview_href}
+                        onChange={handleFormChange}
+                        placeholder="Youtube ID"
+                    />
+                    <TextInput 
+                        name="language"
+                        value={formData.language}
+                        onChange={handleFormChange}
+                        placeholder="Programming Language"
+                    />
+                </FormRow>
+                <FormRow>
+                    <TextAreaInput
+                        name="description"
+                        value={formData.description}
+                        onChange={handleFormChange}
+                        placeholder="Description"
+                    />
+                    <FormSection>
+                        <FormRow>
+                            <CheckBox 
+                                type="checkbox"
+                                name="hasComments?"
+                                id="has-comments"
+                                onChange={handleFormChange}
+                                checked={formData["has_comments?"]}
+                            />
+                            <InputLabel htmlFor="has-comments">
+                                In-code comments?
+                            </InputLabel>
+                        </FormRow>
+                        <FormRow>
+                            <CheckBox 
+                                type="checkbox"
+                                name="keyboard?"
+                                id="keyboard"
+                                onChange={handleFormChange}
+                                checked={formData["keyboard?"]}
+                            />
+                            <InputLabel htmlFor="keyboard">
+                                Keyboard controls?
+                            </InputLabel>
+                        </FormRow>
+                        <FormRow>
+                            <CheckBox 
+                                type="checkbox"
+                                name="controller?"
+                                onChange={handleFormChange}
+                                id="controller"
+                                checked={formData["controller?"]}
+                            />
+                            <InputLabel htmlFor="controller">
+                                Game controller?
+                            </InputLabel>
+                        </FormRow>
+                    </FormSection>
                 </FormRow>
             </FormSection>
             <button type="submit">Submit</button>
